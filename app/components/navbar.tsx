38eeda60fa24a00cc0098dc6e495194f587db489
Anggih Septiawan <albertanggih@gmail.com>
2024-08-17 22:59:12 +0700

    chore: add beta tag and add link to start quiz button

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,8 +2,11 @@ import { Link } from "@remix-run/react"
 
 export const Navbar = () => (
   <nav className="flex justify-between py-5">
-    <Link to="/" className="font-bold">
-      Quizzy
+    <Link to="/" className="flex gap-1 items-center">
+      <span className="font-bold text-xl">Quizzy</span>
+      <small className="bg-indigo-500 rounded-sm px-2 py-[2px] font-bold text-xs">
+        Beta
+      </small>
     </Link>
     <div className="flex gap-8">
       <Link to="/create" className="font-semibold">
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -24,8 +24,8 @@ export default function Index() {
                 variety of engaging topics.
               </p>
               <Link
-                to="#"
-                className="inline-flex h-12 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                to="/create"
+                className="inline-flex h-12 items-center justify-center rounded-md bg-primary px-8 text-sm font-semibold text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                 // prefetch={false}
               >
                 Start Quiz
